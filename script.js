8a6bf6b8903874d7b765e91b9f65f3f156837a42
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2024-11-13 09:58:04 +0200

    added the getHumanChoice function and checks if user entered a valid argument

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+
 //create a function that  radomly returns string values rock,paper or scissors
-//1 create a variable that stores a random number 
-//2 create if statements to generate the strings
-//3 return the strings
 
 function getComputerChoice(){
 
     let rand = Math.random();
 
     if( rand <= 0.33){
-        return "Rock"
+        return "Rock";
 
     } else if(rand > 0.33 && rand <= 0.66){
-        return "Paper"
+        return "Paper";
 
     } else {
-        return "Scissors"
+        return "Scissors";
     }
 }
 
 //console.log(getComputerChoice());
+
+//create a function that gets the users choice and returns it
+
+function getHumanChoice(){
+
+    let choice = prompt("Enter your throw: Rock, Paper, or scissors")
+
+    if (choice.toLowerCase() === "rock"){
+        return "rock";
+
+    } else if(choice.toLowerCase() === "paper"){
+        return "paper";
+
+    } else if (choice.toLowerCase() === "scissor"){
+        return "scissor";
+
+    } else{
+        return "Please enter a valid input";
+    }
+}
+
+//console.log(getHumanChoice());
